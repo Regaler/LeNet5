dece707cba161d8a9b7a9ee9c872542858ea4c44
Regaler <akalsdnr@kaist.ac.kr>
2018-03-23 22:03:44 +0900

    <Softmax activation and CrossEntropyLoss> I made two functions: Softmax activation and CrossEntropyLoss for classification.

--- AllInOne.py
+++ AllInOne.py
@@ -25,53 +25,113 @@ class FC():
 
 	def _forward(self, X):
 		print("FC: _forward")
+		print(X.shape)
+		print(self.W.shape)
 		out = np.dot(X, self.W) + self.b
+		cache = X
 		return out
 
 	def _backward(self, dout, cache):
-		pass
+		X = cache
+		dX = np.dot(dout, self.W.T).reshape(x.shape)
+		dW = np.dot(X.reshape(X.shape[0], np.prod(X.shape[1:])).T, dout)
+		db = np.sum(dout, axis=0)
+		return dX, dW, db
 
 class ReLU():
-	def __init__(self, D_in, D_out):
+	def __init__(self):
 		print("Build ReLU")
-		pass
 
 	def _forward(self, X):
 		out = np.maximum(0, X)
+		cache = X
 		return out
 
 	def _backward(self, dout, cache):
-		pass
+		X = cache
+		dX = np.array(dout, copy=True)
+		dX[X <= 0] = 0
+		return dX
+		
+def Softmax(Y_pred):
+	"""Compute softmax values for each sets of scores in X."""
+	maxes = np.amax(Y_pred, axis=1)
+	maxes = maxes.reshape(maxes.shape[0], 1)
+	e = np.exp(Y_pred - maxes)
+	dist = e / np.sum(e, axis=1).reshape(e.shape[0], 1)
+	#return np.sum(dist, axis=1)
+	#return np.argmax(dist, axis=1)
+	return dist
+
+def CrossEntropyLoss(Y_pred, Y_true):
+	"""
+	
+	"""
+	loss = 0
+	m = Y_pred.shape[0]
+	loss = -(1.0/m) * np.sum(Y_true*np.log(Y_pred) + (1-Y_true)*np.log(1-Y_pred))
+	return loss
 
 class TwoLayerNet():
 	def __init__(self, D_in, H, D_out):
-		self.FC1 = FC(D_in, H)
-		self.FC2 = FC(H, D_out)
+		self.FC1 = FC(N, D_in, H)
+		self.ReLU1 = ReLU()
+		self.FC2 = FC(N, H, D_out)
 
 	def forward(self, X):
-		self.FC1._forward(X)
+		h1 = self.FC1._forward(X)
+		a1 = self.ReLU1._forward(h1)
+		h2 = self.FC2._forward(a1)
+		return h2
 
 	def backward(self):
 		pass
 
-	def loss_function(self):
+	def loss_function(self, Y_pred, Y_true):
 		pass
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# LOSS FUNCTION TEST
+"""
+Y_pred = np.array([[0.1,0.8,0.1],[0.9,0.05,0.05]])
+Y_true = np.array([[1,0,0],[0.5,0.5,0]])
+#Y_pred = np.array([0.1,0.8,0.1])
+#Y_true = np.array([0,1,0])
+loss = CrossEntropyLoss(Y_pred,Y_true)
+print(loss)
+"""
+
+# FC FORWARD TEST
+"""
 myFC = FC(N, D_in, D_out)
 out = myFC._forward(X_train)
 print(out)
-
-
-
-
-
 """
-model = LeNet5()
-for epoch in range(100):
-	Y_pred = model.forward(X_train)
 
-	loss = model.loss_function(Y_pred, Y_train)
-	
-	model.backward(loss)
-
+# TWO LAYER NET FORWARD TEST
+"""
+H = 100
+model = TwoLayerNet(D_in, H, D_out)
+out = model.forward(X_train)
+A = Softmax(out)
+print(A)
 """
